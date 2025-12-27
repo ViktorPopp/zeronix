88a5bb4f50f1872126eb0d94474af3b22f90ec9e
Viktor Popp <viktorpopp.dev@gmail.com>
2025-12-27 16:33:15 +0100

    feat: documentation

---
+++ kernel/docs/source/conf.py
@@ -0,0 +1,36 @@
+# -- Project information -----------------------------------------------------
+
+project = 'Zeronix Kernel'
+copyright = '2025, Viktor Popp'
+author = 'Viktor Popp'
+
+# -- General configuration ---------------------------------------------------
+
+extensions = ['breathe', 'exhale']
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+# -- Exhale configuration ----------------------------------------------------
+
+breathe_projects = {
+    "Kernel": "../output/xml"
+}
+
+breathe_default_project = "Kernel"
+
+exhale_args = {
+    "containmentFolder":     "./api",
+    "rootFileName":          "library_root.rst",
+    "rootFileTitle":         "Library API",
+    "doxygenStripFromPath":  "..",
+    "createTreeView":        True,
+}
+
+primary_domain = 'c'
+highlight_language = 'c'
+
+# -- Options for HTML output -------------------------------------------------
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
